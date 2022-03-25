5126e5b4bad0e5a796be03c05a582309c32d0ddb
manish <manish@gintaa.com>
2022-03-25 06:12:03 +0530

    build command

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,10 @@
+const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
+    entry: path.join(__dirname, "src", "index.js"),
+    output: {
+        path: path.resolve(__dirname, "build"),
+    },
     mode: 'development',
     module: {
         rules: [
@@ -27,7 +32,15 @@ module.exports = {
                     'style-loader',
                     'css-loader'
                 ]
-            }
+            },
+            {
+                test: /\.(png|jp(e*)g|svg|gif)$/,
+                use: ['file-loader'],
+            },
+            {
+                test: /\.svg$/,
+                use: ['@svgr/webpack'],
+            },
         ],
     },
     plugins: [
